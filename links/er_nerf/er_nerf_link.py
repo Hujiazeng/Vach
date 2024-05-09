1bbf1b9d2714e9714d2f2040562480874c5127c9
Home <1>
2024-05-10 00:05:55 +0800

    贴回全身

--- links/er_nerf/er_nerf_link.py
+++ links/er_nerf/er_nerf_link.py
@@ -23,87 +23,10 @@ import pandas as pd
 import torch.nn.functional as F
 
 from core.stream_track import AUDIO_PTIME, SAMPLE_RATE
+from talkers.er_nerf.config import set_opt
 from talkers.er_nerf.nerf_triplane.network import NeRFNetwork
 from talkers.er_nerf.nerf_triplane.provider import nerf_matrix_to_ngp, smooth_camera_path
 from talkers.er_nerf.nerf_triplane.utils import get_bg_coords, get_audio_features, get_rays, seed_everything, Trainer
-
-
-def set_opt(opt):
-    # Custom Settings
-    data_path = os.path.join(opt.base_dir, "talkers", opt.link_name, "data", opt.model_name)
-    opt.asr_model = 'cpierse/wav2vec2-large-xlsr-53-esperanto' # r'C:\Users\k\.cache\huggingface\hub\models--cpierse--wav2vec2-large-xlsr-53-esperanto\snapshots\5ae6c3174dddd1261dd2816eeb6f060655042026'
-    opt.preload = 2
-    opt.pose = os.path.join(data_path, "transforms_train.json")
-    opt.au = os.path.join(data_path, "au.csv")
-    opt.ckpt = os.path.join(data_path, "ngp_kf.pth")
-    opt.template = os.path.join(data_path, "template.npy")
-    opt.bg_img = "white"
-    opt.torso_imgs = ''
-    opt.W = 450
-    opt.H = 450
-
-    # fixed
-    opt.num_rays = 65536
-    opt.fbg = False
-    opt.dt_gamma = 1 / 256  # todo 加速效果非常明显  默认1/256
-    opt.O = False
-    opt.amb_aud_loss = 1
-    opt.amb_dim = 2
-    opt.amb_eye_loss = 1
-    opt.asr = True
-    opt.asr_save_feats = False
-    opt.radius = 3.35
-    opt.seed = 0
-    opt.gui = False
-    opt.fovy = 21.24
-    opt.lambda_amb = 0.0001
-    opt.max_ray_batch = 4096
-    opt.max_spp = 1
-    opt.max_steps = 16
-    opt.part = False
-    opt.part2 = False
-    opt.test = True
-    opt.update_extra_interval = 16
-    opt.upsample_steps = 0
-    opt.warmup_step = 10000
-    opt.fp16 = True
-    opt.color_space = 'srgb'  # 尝试更换
-    opt.fps = 50  # 25??
-    opt.finetune_lips = False
-    opt.asr_wav = ''
-    opt.smooth_path = True
-    opt.cuda_ray = True
-    opt.asr_play = False
-    opt.scale = 4
-    opt.torso_shrink = 0.8
-    opt.l = 10
-    opt.m = 8
-    opt.r = 10
-    opt.fix_eye = -1
-    opt.aud = ''
-    opt.offset = [0, 0, 0]
-    opt.data_range = [0, -1]
-    opt.init_lips = False
-    opt.patch_size = 1
-    opt.exp_eye = True
-    opt.smooth_eye = True
-    opt.asr = True
-    opt.bound = 1
-    opt.min_near = 0.05
-    opt.density_thresh = 10
-    opt.density_thresh_torso = 0.01
-    opt.test_train = False
-    opt.smooth_lips = True
-    opt.smooth_path_window = 7
-    opt.ind_dim = 4
-    opt.ind_num = 10000
-    opt.ind_dim_torso = 8
-    opt.train_camera = False
-    opt.emb = False
-    opt.att = 2
-    if opt.torso_imgs == '':  # no img,use model output
-        opt.torso = True
-    return opt
 
 
 class NerfTestDataset:
@@ -300,6 +223,10 @@ class NerfTestDataset:
                 bg_img = self.bg_img.view(1, -1, 3).repeat(B, 1, 1).to(self.device)
         else:
             bg_img = self.bg_img.view(1, -1, 3).repeat(B, 1, 1).to(self.device)
+
+        # # load full_body
+        # if self.opt.full_body:
+
 
         results['bg_color'] = bg_img
 
@@ -706,7 +633,13 @@ class ErNerfLink:
                 data['auds'] = self.asr.get_next_feat()
                 outputs = self.trainer.test_gui_with_data(data, self.W, self.H)
                 image = (outputs['image'] * 255).astype(np.uint8)
-                video_frame = VideoFrame.from_ndarray(image, format="rgb24")
+                if self.opt.full_body:  # todo dataloader
+                    full_body_img = cv2.imread(os.path.join(self.opt.full_body_imgs, str(data['index'][0])+'.jpg'))
+                    full_body_img = cv2.cvtColor(full_body_img, cv2.COLOR_BGR2RGB)
+                    full_body_img[self.opt.crop_y:self.opt.crop_y + image.shape[0], self.opt.crop_x:self.opt.crop_x + image.shape[1]] = image
+                    video_frame = VideoFrame.from_ndarray(full_body_img, format="rgb24")
+                else:
+                    video_frame = VideoFrame.from_ndarray(image, format="rgb24")
                 asyncio.run_coroutine_threadsafe(self.video_track._queue.put(video_frame), self.loop)
                 # audio submit
                 for _ in range(2):
@@ -765,7 +698,14 @@ class ErNerfLink:
             data['auds'] = self.asr.get_next_feat()
             outputs = self.trainer.test_gui_with_data(data, self.W, self.H)
             image = (outputs['image'] * 255).astype(np.uint8)
-            template_video.append(image)
+            if self.opt.full_body:  # todo dataloader
+                full_body_img = cv2.imread(os.path.join(self.opt.full_body_imgs, str(data['index'][0]) + '.jpg'))
+                full_body_img = cv2.cvtColor(full_body_img, cv2.COLOR_BGR2RGB)
+                full_body_img[self.opt.crop_y:self.opt.crop_y + image.shape[0],
+                self.opt.crop_x:self.opt.crop_x + image.shape[1]] = image
+                template_video.append(full_body_img)
+            else:
+                template_video.append(image)
         self.loader = iter(self.data_loader)
         try:
             np.save(output_path, np.asarray(template_video))
---
+++ talkers/er_nerf/config.py
@@ -0,0 +1,85 @@
+import os
+
+
+def set_opt(opt):
+    # Custom Settings
+    data_path = os.path.join(opt.base_dir, "talkers", opt.link_name, "data", opt.model_name)
+    opt.asr_model = 'cpierse/wav2vec2-large-xlsr-53-esperanto'
+    opt.preload = 2
+    opt.pose = os.path.join(data_path, "transforms_train.json")
+    opt.au = os.path.join(data_path, "au.csv")
+    opt.ckpt = os.path.join(data_path, "ngp_kf.pth")
+    opt.template = os.path.join(data_path, "template.npy")
+    opt.bg_img = "white"
+    opt.torso_imgs = r''
+    opt.W = 450  # 预处理crop时的原始大小
+    opt.H = 450
+
+    # full_body
+    opt.full_body = False
+    opt.full_body_imgs = r''
+    opt.crop_x, opt.crop_y = 0, 0
+
+    # fixed
+    opt.num_rays = 65536
+    opt.fbg = False
+    opt.dt_gamma = 1 / 256  # todo 加速效果非常明显  默认1/256
+    opt.O = False
+    opt.amb_aud_loss = 1
+    opt.amb_dim = 2
+    opt.amb_eye_loss = 1
+    opt.asr = True
+    opt.asr_save_feats = False
+    opt.radius = 3.35
+    opt.seed = 0
+    opt.gui = False
+    opt.fovy = 21.24
+    opt.lambda_amb = 0.0001
+    opt.max_ray_batch = 4096
+    opt.max_spp = 1
+    opt.max_steps = 16
+    opt.part = False
+    opt.part2 = False
+    opt.test = True
+    opt.update_extra_interval = 16
+    opt.upsample_steps = 0
+    opt.warmup_step = 10000
+    opt.fp16 = True
+    opt.color_space = 'srgb'  # 尝试更换
+    opt.fps = 50  # 25??
+    opt.finetune_lips = False
+    opt.asr_wav = ''
+    opt.smooth_path = True
+    opt.cuda_ray = True
+    opt.asr_play = False
+    opt.scale = 4
+    opt.torso_shrink = 0.8
+    opt.l = 10
+    opt.m = 8
+    opt.r = 10
+    opt.fix_eye = -1
+    opt.aud = ''
+    opt.offset = [0, 0, 0]
+    opt.data_range = [0, -1]
+    opt.init_lips = False
+    opt.patch_size = 1
+    opt.exp_eye = True
+    opt.smooth_eye = True
+    opt.asr = True
+    opt.bound = 1
+    opt.min_near = 0.05
+    opt.density_thresh = 10
+    opt.density_thresh_torso = 0.01
+    opt.test_train = False
+    opt.smooth_lips = True
+    opt.smooth_path_window = 7
+    opt.ind_dim = 4
+    opt.ind_num = 10000
+    opt.ind_dim_torso = 8
+    opt.train_camera = False
+    opt.emb = False
+    opt.att = 2
+    opt.torso = False
+    if opt.torso_imgs == '':  # no img,use model output
+        opt.torso = True
+    return opt
